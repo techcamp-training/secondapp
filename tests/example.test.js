f695e9a4bc96d4af0d0116a82291535b60113a6b
Kento Sobayashi <61771591+Kentozzz@users.noreply.github.com>
2024-04-20 22:51:01 +0900

    Revert "no message"
    This reverts commit f4672771effa046ba0cc4109eea5f95ee233a91d.

--- tests/example.test.js
+++
@@ -1,6 +0,0 @@
-describe('Google検索ページ', () => {
-  it('訪れたサイトのタイトルがGoogleであることを確認する', async () => {
-    await page.goto("https://www.google.com");
-    await expect(page.title()).resolves.toMatch("Google");
-  });
-});
--- tests/testUtils.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-const resetDatabase = async () => {
-  await prisma.$executeRaw`TRUNCATE TABLE "Post" RESTART IDENTITY CASCADE;`;
-};
-
-module.exports = { prisma, resetDatabase };
